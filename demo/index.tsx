c904ee165cb43ee98403be00f4ad8e7e281651db
Matt Dunn <matt.j.dunn@gmail.com>
2020-03-13 18:01:03 +0000

    Update demo with some more wireframe annotation components

--- demo/index.tsx
+++ demo/index.tsx
@@ -6,8 +6,15 @@ import { WireFrameContainer, withWireFrameAnnotation } from "src/index";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const Main = styled.div`
+  margin: 10rem;
+`;
+
 const Test = styled.div`
-  border: 2px solid red;
+  border-radius: 0.5rem;
+  padding: 2rem;
+  margin: 0 0 2rem 0;
+  background-color: #f0f0f0;
 `;
 
 const WATest = withWireFrameAnnotation(Test, {
@@ -17,7 +24,12 @@ const WATest = withWireFrameAnnotation(Test, {
 
 const WATest2 = withWireFrameAnnotation(Test, {
   title: "Test component 2",
-  description: "Test component 2 description.",
+  description: "Test component 2 description. Etiam ut sagittis velit, quis hendrerit nisi. Praesent interdum lacinia varius. Phasellus id felis non ex accumsan tempor. Nunc sit amet lobortis enim. Pellentesque lectus nulla, hendrerit dapibus efficitur id, imperdiet sit amet turpis. Maecenas venenatis suscipit finibus. Sed lorem nulla, dictum vel sollicitudin id, posuere eget nulla. Duis accumsan ante eget neque tincidunt pellentesque.",
+});
+
+const WATest3 = withWireFrameAnnotation(Test, {
+  title: "Test component 3",
+  description: "Test component 3 description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
 });
 
 const app = (
@@ -25,9 +37,13 @@ const app = (
     className="container"
     // defaultOpen={false}
   >
-    <WATest>Hello!!</WATest>
-    <WATest2>Hello!!</WATest2>
-    <WATest2>Hello!!</WATest2>
+    <Main>
+      <WATest>Hello</WATest>
+      <WATest2>World</WATest2>
+      <WATest2>World</WATest2>
+      <WATest>Hello</WATest>
+      <WATest3>Hello</WATest3>
+    </Main>
   </WireFrameContainer>
 );
 
