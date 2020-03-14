9dac90f7ffa28315a333400f0711b8ebb3d01450
Matt Dunn <matt.j.dunn@gmail.com>
2020-03-14 16:30:18 +0000

    Update story

--- stories/index.stories.jsx
+++ stories/index.stories.jsx
@@ -16,26 +16,50 @@ const Main = styled.div`
   margin: 10rem;
 `;
 
-const Test = styled.div`
+const Section = styled.div`
   border-radius: 0.5rem;
   padding: 2rem;
   margin: 0 0 2rem 0;
   background-color: #f0f0f0;
 `;
 
-const WATest = withWireFrameAnnotation(Test, {
-  title: "Test component 1",
-  description: "Test component 1 description.",
+const SectionLarge = styled(Section)`
+  min-height: 14rem;
+`;
+
+const WAHeader = withWireFrameAnnotation(Section, {
+  title: "Header",
+  description: "Header description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
 });
 
-const WATest2 = withWireFrameAnnotation(Test, {
-  title: "Test component 2",
-  description: "Test component 2 description. Etiam ut sagittis velit, quis hendrerit nisi. Praesent interdum lacinia varius. Phasellus id felis non ex accumsan tempor. Nunc sit amet lobortis enim. Pellentesque lectus nulla, hendrerit dapibus efficitur id, imperdiet sit amet turpis. Maecenas venenatis suscipit finibus. Sed lorem nulla, dictum vel sollicitudin id, posuere eget nulla. Duis accumsan ante eget neque tincidunt pellentesque.",
+const WAFooter = withWireFrameAnnotation(Section, {
+  title: "Footer",
+  description: "Footer description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
 });
 
-const WATest3 = withWireFrameAnnotation(Test, {
-  title: "Test component 3",
-  description: "Test component 3 description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
+const WASection1 = withWireFrameAnnotation(SectionLarge, {
+  title: "Section 1",
+  description: "Section 1 description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
+});
+
+const WASection2 = withWireFrameAnnotation(SectionLarge, {
+  title: "Section 2",
+  description: "Section 2 description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
+});
+
+const WASection3 = withWireFrameAnnotation(Section, {
+  title: "Section 3",
+  description: "Section 3 description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
+});
+
+const WASection4 = withWireFrameAnnotation(Section, {
+  title: "Section 4",
+  description: "Section 4 description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
+});
+
+const WASection5 = withWireFrameAnnotation(Section, {
+  title: "Section 5",
+  description: "Section 5 description. Morbi tempor libero id accumsan sodales. Etiam maximus convallis faucibus. Nunc hendrerit sit amet ante in lobortis. Aliquam feugiat nibh sit amet nunc varius laoreet. Aliquam pharetra odio mi, sed convallis massa sagittis at. Nullam nibh tortor, commodo ac risus vitae, venenatis lobortis libero.",
 });
 
 export const simpleLayout = () => (
@@ -44,11 +68,35 @@ export const simpleLayout = () => (
     // defaultOpen={false}
   >
     <Main>
-      <WATest>Hello</WATest>
-      <WATest2>World</WATest2>
-      <WATest2>World</WATest2>
-      <WATest>Hello</WATest>
-      <WATest3>Hello</WATest3>
+      <div className="row">
+        <div className="col">
+          <WAHeader>Header</WAHeader>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <WASection1>Section 1</WASection1>
+        </div>
+        <div className="col-6">
+          <WASection2>Section 2</WASection2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <WASection3>Section 3</WASection3>
+        </div>
+        <div className="col-6">
+          <WASection4>Section 4</WASection4>
+        </div>
+        <div className="col">
+          <WASection5>Section 5</WASection5>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <WAFooter>Footer</WAFooter>
+        </div>
+      </div>
     </Main>
   </WireFrameContainer>
 );
