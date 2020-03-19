18cd4ac7afe450e50fc15f873a6f8a67b23d0af6
Matt Dunn <matt.j.dunn@gmail.com>
2020-03-19 15:17:19 +0000

    Created ErrorBoundary test

---
+++ src/ErrorBoundary/__tests__/ErrorBoundary.test.tsx
@@ -0,0 +1,52 @@
+/** !
+ * Copyright (c) 2019, Matt Dunn
+ *
+ * @author Matt Dunn
+ */
+
+import React from "react";
+import { shallow } from "enzyme";
+
+import Component from "../ErrorBoundary";
+
+const SimpleError = ({ error }: {error: Error}) => (
+  <>
+    <h1>An error occurred</h1>
+    <p>
+      DEBUG:
+      {error.message}
+    </p>
+  </>
+);
+
+describe("ErrorBoundary", () => {
+  it("should render correctly", () => {
+    const wrapper = shallow(
+      <Component
+        ErrorComponent={SimpleError}
+      >
+        Simple
+      </Component>,
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should correctly show error", () => {
+    const MockComponent = () => null;
+
+    const wrapper = shallow(
+      <Component
+        ErrorComponent={SimpleError}
+      >
+        <MockComponent />
+      </Component>,
+    );
+
+    const error = new Error("Mock error");
+
+    wrapper.find(MockComponent).simulateError(error);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
