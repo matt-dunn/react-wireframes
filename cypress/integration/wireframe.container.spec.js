c0d88f076a146fdd03ce4fa3ac65b8dc170d6048
Matt Dunn <matt.j.dunn@gmail.com>
2020-03-20 09:35:08 +0000

    Setup configurable cypress base url for CI and dev

--- cypress/integration/wireframe.container.spec.js
+++ cypress/integration/wireframe.container.spec.js
@@ -2,7 +2,7 @@
 
 context("WireframeContainer", () => {
   beforeEach(() => {
-    cy.visit("http://127.0.0.1:8080/");
+    cy.visit("/");
   });
 
   it("Should open and close the annotations", () => {
