eb9e18147535927e18ff91c18151197117ee2794
Matt Dunn <matt.j.dunn@gmail.com>
2020-03-27 16:10:00 +0000

    Use Knobs instead of fixtures

--- src/Wireframes/WireframeAnnotationNotes/WireframeAnnotationNotes.stories.fixtures.js
+++ src/Wireframes/WireframeAnnotationNotes/WireframeAnnotationNotes.stories.fixtures.js
@@ -30,23 +30,3 @@ export const multiple = [
     },
   },
 ];
-
-export const annotationSimple = {
-  id: 1,
-  count: 1,
-  Component: () => {},
-  options: {
-    title: "Test component",
-    description: "Test description",
-  },
-};
-
-export const annotationLongDescription = {
-  id: 1,
-  count: 1,
-  Component: () => {},
-  options: {
-    title: "Test component with long description",
-    description: "Pellentesque non sapien a dolor aliquet tempor sit amet non nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse tristique nulla vitae vestibulum lacinia. Cras sit amet luctus metus. Nunc malesuada erat eu magna dignissim porta.",
-  },
-};
