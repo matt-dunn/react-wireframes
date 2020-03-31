009ec03b6fd7d00ffc9d92752501918be9af3325
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-31 10:47:10 +0000

    Deploy matt-dunn/react-wireframes to github.com/matt-dunn/react-wireframes.git:gh-pages

--- src/Wireframes/WireframeAnnotationNotes/WireframeAnnotationNote.d.ts
+++ src/Wireframes/WireframeAnnotationNotes/WireframeAnnotationNote.d.ts
@@ -7,9 +7,10 @@
 import { WireframeAnnotation } from "../api";
 declare type WireframeAnnotationsNoteProps = {
     annotation: WireframeAnnotation;
+    isHighlighted?: boolean;
 };
 export declare const WireframeAnnotationNote: {
-    ({ annotation }: WireframeAnnotationsNoteProps): JSX.Element;
+    ({ annotation, isHighlighted }: WireframeAnnotationsNoteProps): JSX.Element;
     displayName: string;
     __docgenInfo: {
         description: string;
@@ -17,6 +18,17 @@ export declare const WireframeAnnotationNote: {
         props: {
             annotation: {
                 defaultValue: null;
+                description: string;
+                name: string;
+                required: boolean;
+                type: {
+                    name: string;
+                };
+            };
+            isHighlighted: {
+                defaultValue: {
+                    value: boolean;
+                };
                 description: string;
                 name: string;
                 required: boolean;
