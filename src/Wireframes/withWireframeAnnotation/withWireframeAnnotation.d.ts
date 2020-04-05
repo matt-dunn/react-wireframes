7f191454d0798fa3de8b735d6e67a0819aaf5a66
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-05 11:47:44 +0000

    Deploy matt-dunn/react-wireframes to github.com/matt-dunn/react-wireframes.git:gh-pages

--- src/Wireframes/withWireframeAnnotation/withWireframeAnnotation.d.ts
+++ src/Wireframes/withWireframeAnnotation/withWireframeAnnotation.d.ts
@@ -3,7 +3,7 @@
  *
  * @author Matt Dunn
  */
-import React, { ComponentType, ReactNode } from "react";
+import React, { ReactNode } from "react";
 import { WireframeAnnotationOptions } from "../api";
 declare type WireframeAnnotationProps = {
     className?: string;
@@ -11,21 +11,17 @@ declare type WireframeAnnotationProps = {
     outline?: boolean;
     isHighlighted?: boolean;
 };
-export declare function withWireframeAnnotation<P extends object>(WrappedComponent: ComponentType<P> | string, options: WireframeAnnotationOptions): {
+export declare function withWireframeAnnotation(options: WireframeAnnotationOptions): <P extends object>(WrappedComponent: "symbol" | "object" | "style" | "title" | "a" | "abbr" | "address" | "area" | "article" | "aside" | "audio" | "b" | "base" | "bdi" | "bdo" | "big" | "blockquote" | "body" | "br" | "button" | "canvas" | "caption" | "cite" | "code" | "col" | "colgroup" | "data" | "datalist" | "dd" | "del" | "details" | "dfn" | "dialog" | "div" | "dl" | "dt" | "em" | "embed" | "fieldset" | "figcaption" | "figure" | "footer" | "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "head" | "header" | "hgroup" | "hr" | "html" | "i" | "iframe" | "img" | "input" | "ins" | "kbd" | "keygen" | "label" | "legend" | "li" | "link" | "main" | "map" | "mark" | "menu" | "menuitem" | "meta" | "meter" | "nav" | "noindex" | "noscript" | "ol" | "optgroup" | "option" | "output" | "p" | "param" | "picture" | "pre" | "progress" | "q" | "rp" | "rt" | "ruby" | "s" | "samp" | "script" | "section" | "select" | "small" | "source" | "span" | "strong" | "sub" | "summary" | "sup" | "table" | "template" | "tbody" | "td" | "textarea" | "tfoot" | "th" | "thead" | "time" | "tr" | "track" | "u" | "ul" | "var" | "video" | "wbr" | "webview" | "svg" | "animate" | "animateMotion" | "animateTransform" | "circle" | "clipPath" | "defs" | "desc" | "ellipse" | "feBlend" | "feColorMatrix" | "feComponentTransfer" | "feComposite" | "feConvolveMatrix" | "feDiffuseLighting" | "feDisplacementMap" | "feDistantLight" | "feDropShadow" | "feFlood" | "feFuncA" | "feFuncB" | "feFuncG" | "feFuncR" | "feGaussianBlur" | "feImage" | "feMerge" | "feMergeNode" | "feMorphology" | "feOffset" | "fePointLight" | "feSpecularLighting" | "feSpotLight" | "feTile" | "feTurbulence" | "filter" | "foreignObject" | "g" | "image" | "line" | "linearGradient" | "marker" | "mask" | "metadata" | "mpath" | "path" | "pattern" | "polygon" | "polyline" | "radialGradient" | "rect" | "stop" | "switch" | "text" | "textPath" | "tspan" | "use" | "view" | React.ComponentClass<P, any> | React.FunctionComponent<P>) => {
     ({ className, outline, isHighlighted, ...props }: P & WireframeAnnotationProps): React.ReactElement<P, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
     Component: React.NamedExoticComponent<P>;
 };
-export declare const withWireframeAnnotationInterfaceDefinition: {
-    <P extends object>({ WrappedComponent, options }: {
-        WrappedComponent: React.ComponentType<P>;
-        options: WireframeAnnotationOptions;
-    }): null;
-    displayName: string;
-    __docgenInfo: {
+export declare namespace withWireframeAnnotation {
+    var displayName: string;
+    var __docgenInfo: {
         description: string;
         displayName: string;
         props: {
-            WrappedComponent: {
+            title: {
                 defaultValue: null;
                 description: string;
                 name: string;
@@ -34,7 +30,28 @@ export declare const withWireframeAnnotationInterfaceDefinition: {
                     name: string;
                 };
             };
-            options: {
+            description: {
+                defaultValue: null;
+                description: string;
+                name: string;
+                required: boolean;
+                type: {
+                    name: string;
+                };
+            };
+        };
+    };
+}
+export declare const withWireframeAnnotationInterfaceDefinition: {
+    (params: {
+        withWireframeAnnotation: (options: WireframeAnnotationOptions) => <P extends object>(WrappedComponent: "symbol" | "object" | "style" | "title" | "a" | "abbr" | "address" | "area" | "article" | "aside" | "audio" | "b" | "base" | "bdi" | "bdo" | "big" | "blockquote" | "body" | "br" | "button" | "canvas" | "caption" | "cite" | "code" | "col" | "colgroup" | "data" | "datalist" | "dd" | "del" | "details" | "dfn" | "dialog" | "div" | "dl" | "dt" | "em" | "embed" | "fieldset" | "figcaption" | "figure" | "footer" | "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "head" | "header" | "hgroup" | "hr" | "html" | "i" | "iframe" | "img" | "input" | "ins" | "kbd" | "keygen" | "label" | "legend" | "li" | "link" | "main" | "map" | "mark" | "menu" | "menuitem" | "meta" | "meter" | "nav" | "noindex" | "noscript" | "ol" | "optgroup" | "option" | "output" | "p" | "param" | "picture" | "pre" | "progress" | "q" | "rp" | "rt" | "ruby" | "s" | "samp" | "script" | "section" | "select" | "small" | "source" | "span" | "strong" | "sub" | "summary" | "sup" | "table" | "template" | "tbody" | "td" | "textarea" | "tfoot" | "th" | "thead" | "time" | "tr" | "track" | "u" | "ul" | "var" | "video" | "wbr" | "webview" | "svg" | "animate" | "animateMotion" | "animateTransform" | "circle" | "clipPath" | "defs" | "desc" | "ellipse" | "feBlend" | "feColorMatrix" | "feComponentTransfer" | "feComposite" | "feConvolveMatrix" | "feDiffuseLighting" | "feDisplacementMap" | "feDistantLight" | "feDropShadow" | "feFlood" | "feFuncA" | "feFuncB" | "feFuncG" | "feFuncR" | "feGaussianBlur" | "feImage" | "feMerge" | "feMergeNode" | "feMorphology" | "feOffset" | "fePointLight" | "feSpecularLighting" | "feSpotLight" | "feTile" | "feTurbulence" | "filter" | "foreignObject" | "g" | "image" | "line" | "linearGradient" | "marker" | "mask" | "metadata" | "mpath" | "path" | "pattern" | "polygon" | "polyline" | "radialGradient" | "rect" | "stop" | "switch" | "text" | "textPath" | "tspan" | "use" | "view" | React.ComponentClass<P, any> | React.FunctionComponent<P>) => React.ReactElement<WireframeAnnotationProps, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
+    }): null;
+    displayName: string;
+    __docgenInfo: {
+        description: string;
+        displayName: string;
+        props: {
+            withWireframeAnnotation: {
                 defaultValue: null;
                 description: string;
                 name: string;
