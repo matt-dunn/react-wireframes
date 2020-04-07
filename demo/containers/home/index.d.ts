c4e5064c4a170003aa28ba836e62ecd1ec3c4054
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-07 16:36:18 +0000

    Deploy matt-dunn/react-wireframes to github.com/matt-dunn/react-wireframes.git:gh-pages

---
+++ demo/containers/home/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+import "./styles/main.scss";
+export declare const Home: () => JSX.Element;
---
+++ src/useLocalStorage.d.ts
@@ -0,0 +1,1 @@
+export declare function useLocalStorage<P>(key: string, initial?: P | undefined): [P | undefined, (newValue: P | undefined) => void];
